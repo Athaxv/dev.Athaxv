1c611889db3432cf8a1f4a331d07af948f3a75ea
AtharvGaur <atharava112@gmail.com>
2026-02-21 00:29:23 +0530

    Fixed private section

--- src/app/(app)/onlyfans/page.tsx
+++ src/app/(app)/onlyfans/page.tsx
@@ -2,7 +2,7 @@ export default function Page() {
   return (
     <div className="flex min-h-[50vh] flex-col items-center justify-center">
       <h1 className="font-mono text-2xl font-bold">
-        <span className="underline underline-offset-4">cumming</span> soon!
+        <span className="underline underline-offset-4">coming</span> soon!
       </h1>
     </div>
   );
